55769ec7a3a7d3bd81347a9eb11d8c14b1a73a69
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-15 23:10:18 +0200

    apporté les modifications

--- find_string.sh
+++ find_string.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Ce script recherche une chaîne de caractères spécifique dans un fichier
-echo "entrez le nom du fichier :"
 read fichier
 #stock le nom dans la variable 'fichier'
 
@@ -8,9 +7,7 @@ read chaine
 #stock le nom dans la variable 'chaine'
 
 if grep -q "$chaine" "$fichier"; then
-    grep "$chaine" "$fichier"
     echo "la chaine '$chaine' a été trouvée dans $fichier."
 else
     echo "la chaine '$chaine' n'a pas été trouvée dans $fichier." 
 fi 
-exit 0 
