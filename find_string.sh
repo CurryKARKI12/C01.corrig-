ac78cb409f1dbdf18c35a001df9bd77056cbeb44
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-16 15:48:08 +0200

    find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -2,22 +2,20 @@
 # Indique que ce script doit être exécuté avec l’interpréteur Bash
 
 # Demander le nom du fichier à l'utilisateur
-read fichier
+read f
 # Stocke la saisie dans la variable 'fichier'
 
 # Demander la chaîne de caractères à rechercher dans le fichier
-read chaine
+read c
 # Stocke la saisie dans la variable 'chaine'
 
 # Recherche de la chaîne dans le fichier à l’aide de grep
-if grep -q "$chaine" "$fichier"; then
+if grep -q "$c" "$f"; then
+    echo "La chaîne '$c' a été trouvée dans $f."
     # L’option -q permet de faire une recherche silencieuse (sans affichage), juste pour tester
-    grep "$chaine" "$fichier"
-    # Si la chaîne est trouvée, on affiche toutes les lignes qui la contiennent
 else
-    echo "La chaîne '$chaine' n’a pas été trouvée dans $fichier."
+    echo "La chaîne '$c' n'a pas été trouvée dans $f."
     # Si la chaîne n’est pas trouvée, on affiche un message d’information
 fi
-
 # Fin normale du script
 exit 0
